3a8995c38e37e2fff9b9833b2e0a0590f7555637
Aris2049 <ningxin1718@gmail.com>
2025-12-26 11:00:22 +0800

    fix: unable collect tx check

--- x/genutil/collect.go
+++ x/genutil/collect.go
@@ -8,14 +8,13 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"runtime"
 	"sort"
 	"strings"
 	"time"
-
+	
 	cfg "github.com/cometbft/cometbft/config"
 	tmtypes "github.com/cometbft/cometbft/types"
-
+	
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -164,37 +163,37 @@ func CollectTxs(cdc codec.JSONCodec, txJSONDecoder sdk.TxDecoder, moniker, genTx
 		msg := msgs[0].(*stakingtypes.MsgCreateValidator)
 
 		// validate delegator and validator addresses and funds against the accounts in the state
-		delAddr := msg.DelegatorAddress
-		valAddr, err := sdk.ValAddressFromBech32(msg.ValidatorAddress)
-		if err != nil {
-			return appGenTxs, persistentPeers, err
-		}
+		//delAddr := msg.DelegatorAddress
+		//valAddr, err := sdk.ValAddressFromBech32(msg.ValidatorAddress)
+		//if err != nil {
+		//	return appGenTxs, persistentPeers, err
+		//}
 
-		delBal, delOk := balancesMap[delAddr]
-		if !delOk {
-			_, file, no, ok := runtime.Caller(1)
-			if ok {
-				fmt.Printf("CollectTxs-1, called from %s#%d\n", file, no)
-			}
+		//delBal, delOk := balancesMap[delAddr]
+		//if !delOk {
+		//	_, file, no, ok := runtime.Caller(1)
+		//	if ok {
+		//		fmt.Printf("CollectTxs-1, called from %s#%d\n", file, no)
+		//	}
+		//
+		//	return appGenTxs, persistentPeers, fmt.Errorf("account %s balance not in genesis state: %+v", delAddr, balancesMap)
+		//}
 
-			return appGenTxs, persistentPeers, fmt.Errorf("account %s balance not in genesis state: %+v", delAddr, balancesMap)
-		}
+		//_, valOk := balancesMap[sdk.AccAddress(valAddr).String()]
+		//if !valOk {
+		//	_, file, no, ok := runtime.Caller(1)
+		//	if ok {
+		//		fmt.Printf("CollectTxs-2, called from %s#%d - %s\n", file, no, sdk.AccAddress(msg.ValidatorAddress).String())
+		//	}
+		//	return appGenTxs, persistentPeers, fmt.Errorf("account %s balance not in genesis state: %+v", valAddr, balancesMap)
+		//}
 
-		_, valOk := balancesMap[sdk.AccAddress(valAddr).String()]
-		if !valOk {
-			_, file, no, ok := runtime.Caller(1)
-			if ok {
-				fmt.Printf("CollectTxs-2, called from %s#%d - %s\n", file, no, sdk.AccAddress(msg.ValidatorAddress).String())
-			}
-			return appGenTxs, persistentPeers, fmt.Errorf("account %s balance not in genesis state: %+v", valAddr, balancesMap)
-		}
-
-		if delBal.GetCoins().AmountOf(msg.Value.Denom).LT(msg.Value.Amount) {
-			return appGenTxs, persistentPeers, fmt.Errorf(
-				"insufficient fund for delegation %v: %v < %v",
-				delBal.GetAddress().String(), delBal.GetCoins().AmountOf(msg.Value.Denom), msg.Value.Amount,
-			)
-		}
+		//if delBal.GetCoins().AmountOf(msg.Value.Denom).LT(msg.Value.Amount) {
+		//	return appGenTxs, persistentPeers, fmt.Errorf(
+		//		"insufficient fund for delegation %v: %v < %v",
+		//		delBal.GetAddress().String(), delBal.GetCoins().AmountOf(msg.Value.Denom), msg.Value.Amount,
+		//	)
+		//}
 
 		// exclude itself from persistent peers
 		if msg.Description.Moniker != moniker {
